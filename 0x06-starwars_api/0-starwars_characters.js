d71c0260b31552aae698c4fa2ea432f13173a886
CHIGODI <chiegody254@gmail.com>
2024-08-07 16:15:09 +0300

    use axios add readme

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,28 @@
 #!/usr/bin/node
 
 const request = require('request');
+const axios = require('axios');
 
 const movieID = process.argv[2];
 
-request(`https://swapi-api.alx-tools.com/api/films/${movieID}`, function (error, response, body) {
-  if (error) {
-    console.error('Error fetching character:', error);
-    return;
-  }
-  const films = JSON.parse(body);
-  const characters = films.characters;
+request(`https://swapi-api.alx-tools.com/api/films/${movieID}/`,
+  function (error, response, body) {
+    if (error) {
+      console.error('Error fetching film data:', error);
+      return;
+    }
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-  characters.forEach(character => {
-    request(character, function (error, res, body) {
-      if (error) {
-        console.error('Error fetching character:', error);
-        return;
+    characters.forEach(character => {
+      async function getCharacter () {
+        try {
+          const response = await axios.get(character);
+          console.log(response.data.name);
+        } catch (error) {
+          console.error('Error fetching character:', error);
+        }
       }
-      const character = JSON.parse(body);
-      console.log(character.name);
+      getCharacter();
     });
   });
-});
